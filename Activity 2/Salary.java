8c4e8b17ae1ef39474ed1798b4e3498cf9190682
Aldrick <42536006+ALDXMARIX@users.noreply.github.com>
2019-03-08 08:50:57 +0800

    Add files via upload

--- Activity 2/Salary.java
+++ Activity 2/Salary.java
@@ -1,33 +1,30 @@
 import java.util.Scanner;
 
-
 public class Salary {
-    
-    
-    public static void main(String[] args){
 
-    Scanner sc = new Scanner(System.in);
-    System.out.println("Enter hourly pay rate: " );
-    double  hourlyRate = sc.nextDouble();
+	public static void main(String[] args) {
 
-    System.out.println("Enter regular hours of work: " );
-    double regularHours = sc.nextDouble();
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter hourly pay rate: ");
+		double hourlyRate = sc.nextDouble();
 
-    System.out.println("Enter overtime hours: " );
-    double overtimeHours = sc.nextDouble();
+		System.out.println("Enter regular hours of work: ");
+		double regularHours = sc.nextDouble();
 
-    System.out.println("Overtime Pay: " + overtimePay(hourlyRate,  regularHours, overtimeHours));
+		System.out.println("Enter overtime hours: ");
+		double overtimeHours = sc.nextDouble();
 
-  
-    
-    }
-    
-    
-    static double  overtimePay(double hourlyRate, double regularHours, double overtimeHours){
-    
-    return (regularHours * hourlyRate + ((overtimeHours * 1.5)*regularHours));
-    }
-    
-    
-    
+		System.out.println("Normal Salary: " + hourlyRate * regularHours);
+		double overtimeSalary = calculateOvertimePay(hourlyRate, regularHours, overtimeHours)
+				- hourlyRate * regularHours;
+		System.out.println("Overtime Pay: " + overtimeSalary);
+		System.out.println("Total Salary: " + calculateOvertimePay(hourlyRate, regularHours, overtimeHours));
+
+	}
+
+	static double calculateOvertimePay(double hourlyRate, double regularHours, double overtimeHours) {
+
+		return (regularHours * hourlyRate + (overtimeHours * (1.5 * regularHours)));
+	}
+
 }
